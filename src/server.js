7f95791e34362dfa881db5f218c44578f0d36df4
Henrique Brites <henriquebrites@live.com>
2020-03-02 00:38:04 -0300

    Completed the api cors, with relationships, with mongodb database using mongooseand atlas, uploading image to the api, with routs, models, controllers.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+  }
+);
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
