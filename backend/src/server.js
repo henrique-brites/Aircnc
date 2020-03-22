1c16fc5fbed750d0ba5e6eeb94c6a406321968b2
Henrique Brites <henriquebrites@live.com>
2020-03-22 05:04:03 -0300

    Socketio implemented on the backend, frontend and mobile

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   process.env.MONGO_URL,
@@ -18,21 +23,25 @@ mongoose.connect(
   }
 );
 
-const logRequestStart = (req, res, next) => {
-  console.info(`${req.method} ${req.originalUrl}`) 
-  
-  res.on('finish', () => {
-      console.info(`${res.statusCode} ${res.statusMessage}; ${res.get('Content-Length') || 0}b sent`)
-  })
-  
-  next()
-}
+const connectedUsers = {};
 
-app.use(logRequestStart)
+io.on('connection', socket => {
+
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
 
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
